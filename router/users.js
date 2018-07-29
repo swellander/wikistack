772ae58e6f7f25d46c7e62a8ecb2d0fb43a9b99b
Sam Wellander <samwellander@gmail.com>
2018-07-29 14:44:48 -0400

    user route

--- router/users.js
+++ router/users.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { User, Page } = require('../db');
 const { userList, userPages } = require('../views');
 
+router.get('/', (req, res, next) => {
+  User.findAll()
+    .then(users => res.send(userList(users)))
+    .catch(next);
+});
 
+router.get('/:id', async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  const pages = await Page.findAll({ where: { authorId: user.id } })
+  res.send(userPages(user, pages));
+})
 
 module.exports = router;
